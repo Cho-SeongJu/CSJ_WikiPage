cb245a8b1f773b51e84d8501829240053623ba39
Cho-SeongJu <tjdwn9753@gmail.com>
2024-02-25 22:38:33 +0900

    ➕ dependency : react-query, query devtool 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,24 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Router.tsx";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1 * 60 * 30000,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
       <Router />
-    </BrowserRouter>
-  </React.StrictMode>
+    </QueryClientProvider>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
